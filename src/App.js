ff41e260754a02126eef30ddbfc6ed46d5f065d2
Emmanuel <semashat@gmail.com>
2023-03-19 23:31:37 +0100

    Another commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Nav/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      
+   <BrowserRouter>
+     <div className="App">
+      <Navbar />
     </div>
+   </BrowserRouter>
+
   );
 }
 
---
+++ src/Components/Nav/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link}  from "react-router-dom";
+import '../Nav/Navbar.css';
+
+const Navbar = () => {
+    return(
+        <>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Home</Link>
+        </>
+    );
+} 
+
+export default Navbar;
