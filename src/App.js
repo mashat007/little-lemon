2ee961ac9fcb0a36007b6f1e313a8fd12cbd0bde
Emmanuel <semashat@gmail.com>
2023-03-20 22:02:14 +0100

    Another commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Nav/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Services from './Pages/Services';
 
 
 function App() {
@@ -10,6 +13,11 @@ function App() {
      <div className="App">
       <Navbar />
     </div>
+    <Routes>
+      <Route path='/' exact element={<Home />}/>
+      <Route path='/about' exact element={<About />}/>
+      <Route path='/services' exact element={<Services />}/>
+    </Routes>
    </BrowserRouter>
 
   );
--- src/Components/Nav/Navbar.jsx
+++ src/Components/Nav/Navbar.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
-import {Link}  from "react-router-dom";
+// import styled from 'styled-components';
+import {Link, NavLink}  from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
 import '../Nav/Navbar.css';
 
 const Navbar = () => {
+    const handleClick = () =>{
+        alert("Clicked")
+    }
     return(
         <>
-            <Link to='/'>Home</Link>
-            <Link to='/'>Home</Link>
-            <Link to='/'>Home</Link>
-            <Link to='/'>Home</Link>
+            <nav className='nav'>
+                <NavLink to="/" className="navLink">
+                    Logo
+                </NavLink>
+                <div className="hamburger">
+                    <MenuIcon className='hamburger-icon' onClick={handleClick} />
+                    {/* <ClearIcon className='hamburger-icon' /> */}
+                </div>
+                <div className="navMenu">
+                    <NavLink to="/about" className="navLink"  activeStyle >
+                        About
+                    </NavLink>
+                    <NavLink to="/services" className="navLink" activeStyle>
+                        Services
+                    </NavLink>
+                    <NavLink to="/contact" className="navLink" activeStyle>
+                        Contact Us
+                    </NavLink>
+                    <NavLink to="/sign-up" className="navLink" activeStyle>
+                        Sign up
+                    </NavLink>
+                </div>
+                <div className="signin">
+                    <NavLink to="/sign-in" className="navLink" activeStyle>
+                        Sign-in
+                    </NavLink>
+                </div>
+            </nav>
+          
         </>
     );
 } 
